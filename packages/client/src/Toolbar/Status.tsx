510eeb38ec94a91bfc5f4ce3c86fd3e256e0d38e
Lee Goddard <leegee@gmail.com>
2024-04-04 00:05:53 +0200

    Include to/from

--- packages/client/src/Toolbar/Status.tsx
+++ packages/client/src/Toolbar/Status.tsx
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { get } from 'react-intl-universal';
 import { useSelector } from 'react-redux';
 
-import { selectClusterCount, selectPointsCount } from '../redux/mapSlice';
+import type { RootState } from '../redux/store';
+import { selectPointsCount } from '../redux/mapSlice';
 
 import './Status.css';
 
+const DATE_LOCALE_OPTIONS: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+
 const Panel: React.FC = () => {
     const pointsCount = useSelector(selectPointsCount);
-    const clusterCount = useSelector(selectClusterCount);
+    const { from_date, to_date } = useSelector((state: RootState) => state.map);
     const [nothingToShow, setNothingToShow] = useState<boolean>(true);
-    const showPoints = !clusterCount && pointsCount && pointsCount > 0;
+    const showPoints = pointsCount > 0;
 
     useEffect(() => {
-        setNothingToShow(!clusterCount && !pointsCount);
-    }, [pointsCount, clusterCount, nothingToShow]);
+        setNothingToShow(!pointsCount);
+    }, [pointsCount, nothingToShow]);
 
     return (
         <header className='report-ctrl component'>
@@ -22,12 +31,27 @@ const Panel: React.FC = () => {
                 {nothingToShow ? (
                     <>{get('status.no_results')}</>
                 ) : showPoints ? (
-                    <>
-                        {get('status.points_count', { count: pointsCount })}
-                    </>
+                    <> {get('status.points_count', { count: pointsCount })} </>
                 ) : (
+                    <> Unexpected state </>
+                )}
+
+                {from_date > 0 && (
                     <>
-                        {get('status.cluster_count', { count: clusterCount })}
+                        {get('from')}
+                        <time dateTime={new Date(from_date).toISOString()} >
+                            {new Date(from_date).toLocaleString([], DATE_LOCALE_OPTIONS)
+                            }
+                        </time>
+                    </>
+                )}
+
+                {to_date > 0 && get('to') && (
+                    <>
+                        {get('to')}
+                        <time dateTime={new Date(to_date).toISOString()}>
+                            {new Date(to_date).toLocaleString()}
+                        </time>
                     </>
                 )}
             </span>
