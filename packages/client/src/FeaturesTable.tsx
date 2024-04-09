1573f57e548a67cbb74406048d6e57051f0b8a1f
Lee Goddard <leegee@gmail.com>
2024-04-09 13:05:54 +0200

    Use AG table for speed

--- packages/client/src/FeaturesTable.tsx
+++ packages/client/src/FeaturesTable.tsx
@@ -1,147 +1,102 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* Create the interface for feature.properties */
-import React, { useEffect, useRef, useState } from 'react';
-import { get } from 'react-intl-universal';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { AgGridReact } from 'ag-grid-react';
+import { get } from 'react-intl-universal';
 
 import config from '@hessdalen-sensor-map/config/src';
 import { RootState } from './redux/store';
 import { setPanel, setSelectionId } from './redux/guiSlice';
 
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.min.css';
 import './FeatureTable.css';
-
-function getRowId(id: number | string) {
-    return 'fid_' + id;
-}
-
-function getIdFromRow_id(id: number | string) {
-    return Number(String(id).substring(4));
-}
 
 const FeatureTable: React.FC = () => {
     const dispatch = useDispatch();
     const { featureCollection } = useSelector((state: RootState) => state.map);
-    const { selectionId } = useSelector((state: RootState) => state.gui);
-    const [localFeatures, setLocalFeatures] = useState<any[]>([]);
-    const selectedRowRef = useRef<HTMLTableRowElement>(null);
-
-    function handleClickRow(id: number) {
-        dispatch(setSelectionId(Number(id)));
-    }
-
-    function handleKeyDown(event: KeyboardEvent) {
-        if (!selectionId || !featureCollection?.features) {
-            return;
-        }
-        const selectedRow = document.querySelector('#' + getRowId(selectionId));
-
-        if (event.key === 'ArrowUp') {
-            const previousRow = selectedRow ? selectedRow.previousElementSibling : null;
-            if (previousRow?.id) {
-                dispatch(setSelectionId(getIdFromRow_id(previousRow.id)));
-            }
-        } else if (event.key === 'ArrowDown') {
-            const nextRow = selectedRow ? selectedRow.nextElementSibling : null;
-            if (nextRow?.id) {
-                dispatch(setSelectionId(getIdFromRow_id(nextRow.id)));
-            }
-        } else if (event.key === 'PageUp') {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            const firstRowId = featureCollection.features.length > 0 ? featureCollection.features[0].id : null;
-            dispatch(setSelectionId(getIdFromRow_id(firstRowId)));
-        } else if (event.key === 'PageDown') {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            const lastRowId = featureCollection.features.length > 0 ? featureCollection.features[featureCollection.features.length - 1].id : null;
-            if (lastRowId) {
-                dispatch(setSelectionId(getIdFromRow_id(lastRowId)));
-            }
-        }
-    }
+    // const { selectionId } = useSelector((state: RootState) => state.gui);
+    const gridRef = useRef<AgGridReact>(null);
 
     useEffect(() => {
-        document.addEventListener('keydown', handleKeyDown);
-        return () => document.removeEventListener('keydown', handleKeyDown);
-    });
+        if (gridRef.current?.api) {
+            gridRef.current.api.addEventListener('rowClicked', (event: CustomEvent) => {
+                dispatch(setSelectionId((event.data).id));
+            });
+        }
+    }, [dispatch]);
 
-    // Scroll the selected row into view when user selectionchanges, if it is not already visible
-    useEffect(() => {
-        if (selectedRowRef.current) {
-            const rect = selectedRowRef.current.getBoundingClientRect();
-            if (rect.top < 0 || rect.bottom > window.innerHeight) {
-                selectedRowRef.current.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+    const ActionsRenderer = (props: any) => (
+        <>
+            <span className='ctrl row-goto-map' onClick={() => props.onMapClick(props.data.id)} />
+            <Link className='ctrl row-goto-details' to={props.onDetailsClick(props.data.id)} />
+        </>
+    );
+
+    const onGridReady = (params: any) => {
+        params.api.cellRendererService.registerRenderer('actionsRenderer', ActionsRenderer);
+    };
+
+    const columnDefs = [
+        {
+            headerName: get('feature_table.date'),
+            field: 'timestamp',
+            valueFormatter: (params: any) => {
+                if (params.value) {
+                    return new Intl.DateTimeFormat(config.locale, {
+                        year: undefined,
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit'
+                    }).format(new Date(String(params.value)));
+                }
+                return '';
+            }
+        },
+        { headerName: get('feature_table.mag_x'), field: 'mag_x', type: 'number', valueFormatter: (params: any) => params.value.toFixed(4) },
+        { headerName: get('feature_table.mag_y'), field: 'mag_y', type: 'number', valueFormatter: (params: any) => params.value.toFixed(4) },
+        { headerName: get('feature_table.mag_z'), field: 'mag_z', type: 'number', valueFormatter: (params: any) => params.value.toFixed(4) },
+        { headerName: get('feature_table.rc_temperature'), field: 'rc_temperature', type: 'number', valueFormatter: (params: any) => params.value.toFixed(4) },
+        {
+            headerName: '',
+            cellRenderer: 'actionsRenderer',
+            cellRendererParams: {
+                onMapClick: (id: number) => {
+                    dispatch(setPanel('narrow'));
+                    dispatch(setSelectionId(id));
+                },
+                onDetailsClick: (id: number) => `/sighting/${id}`
             }
         }
-    }, [selectionId]);
+    ];
 
-    useEffect(() => {
-        if (featureCollection) {
-            if (featureCollection.features !== null) {
-                setLocalFeatures(featureCollection.features);
-            }
-        }
-    }, [featureCollection]);
+    const defaultColDef = {
+        sortable: true,
+        resizable: true,
+        flex: 1
+    };
 
-    function getRowClass(id: number) {
-        return selectionId && selectionId === Number(id) ? 'tr selected' : 'tr';
-    }
-
-    function showPointOnMap(feature: any) {
-        dispatch(setPanel('narrow'));
-        dispatch(setSelectionId(Number(feature.properties.id)));
-    }
+    const rowData = featureCollection?.features.map((feature: any) => ({
+        ...feature.properties,
+        id: feature.properties.id
+    })) || [];
 
     return (
-        <table id='feature-table'>
-            <thead className='thead'>
-                <tr className='tr'>
-                    {localFeatures.length && localFeatures[0].properties.timestamp
-                        ? (<th className='th timestamp'>{get('feature_table.date')}</th>)
-                        : ''
-                    }
-                    <th className='th number'>{get('feature_table.mag_x')}</th>
-                    <th className='th number'>{get('feature_table.mag_y')}</th>
-                    <th className='th number'>{get('feature_table.mag_z')}</th>
-                    <th className='th number'>{get('feature_table.rc_temperature')}</th>
-                    <th className='th ctrls'> </th>
-                </tr>
-            </thead>
-
-            <tbody className='tbody'>
-                {localFeatures
-                    .map((feature: any, index: number) => (
-
-                        <tr className={getRowClass(feature.properties.id)}
-                            ref={feature.properties.id === selectionId ? selectedRowRef : null}
-                            key={index} id={getRowId(feature.properties.id)}
-                            onClick={() => handleClickRow(feature.properties.id)}
-                        >
-                            {feature.properties.timestamp &&
-                                <td className='td timestamp'>
-                                    {new Intl.DateTimeFormat(config.locale, {
-                                        year: undefined,
-                                        month: '2-digit',
-                                        day: '2-digit',
-                                        hour: '2-digit',
-                                        minute: '2-digit',
-                                        second: '2-digit'
-                                    }).format(new Date(String(feature.properties.timestamp)))}
-                                </td>
-                            }
-                            <td className='td number'>{Number(feature.properties.mag_x ?? -999).toFixed(4)}</td>
-                            <td className='td number'>{Number(feature.properties.mag_y ?? -999).toFixed(4)}</td>
-                            <td className='td number'>{Number(feature.properties.mag_z ?? -999).toFixed(4)}</td>
-                            <td className='td number'>{Number(feature.properties.rc_temperature ?? -999).toFixed(4)}</td>
-                            <td className='td ctrls'>
-                                <span className='ctrl row-goto-map' onClick={() => showPointOnMap(feature)} />
-                                <Link className='ctrl row-goto-details' to={'/sighting/' + feature.properties.id} />
-                            </td>
-                        </tr>
-                    ))}
-            </tbody>
-        </table>
+        <div className="ag-theme-quartz-auto-dark" style={{ height: '95%', width: '100%' }}>
+            {rowData.length ?
+                <AgGridReact
+                    ref={gridRef}
+                    columnDefs={columnDefs}
+                    rowData={rowData}
+                    defaultColDef={defaultColDef}
+                    onGridReady={onGridReady}
+                ></AgGridReact>
+                : ''
+            }
+        </div>
     );
 };
 
 export default FeatureTable;
-
--- packages/client/src/ResultsPanel.tsx
+++ packages/client/src/ResultsPanel.tsx
@@ -5,7 +5,7 @@
 import React, { useEffect, useState } from 'react';
 import { get } from 'react-intl-universal';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectClusterCount, selectPointsCount } from './redux/mapSlice';
+import { selectPointsCount } from './redux/mapSlice';
 import { RootState } from './redux/store';
 import { setPanel } from './redux/guiSlice'
 import FeatureTable from './FeaturesTable';
@@ -16,7 +16,6 @@ import './ResultsPanel.css';
 const Panel: React.FC = () => {
     const dispatch = useDispatch();
     const pointsCount = useSelector(selectPointsCount);
-    const clusterCount = useSelector(selectClusterCount);
     const { isAnimating } = useSelector((state: RootState) => state.gui);
 
     const [nothingToShow, setNothingToShow] = useState<boolean>(true);
@@ -32,31 +31,27 @@ const Panel: React.FC = () => {
         if (pointsCount) {
             setPanel(nothingToShow ? 'narrow' : 'hidden')
         }
-        setNothingToShow(!clusterCount && !pointsCount);
+        setNothingToShow(!pointsCount);
 
-    }, [pointsCount, clusterCount, nothingToShow]);
+    }, [pointsCount, nothingToShow]);
 
     return (
-        isAnimating
-            ? ''
-            : (
-                <section id='panel' className={(nothingToShow || clusterCount) ? 'nothing-to-show' : ''}>
-                    {nothingToShow ? (
-                        <p className='message'>
-                            {get('panel.no_results')}
-                        </p>
-                    ) :
-                        pointsCount ?
-                            <>
-                                <ReportButton />
-                                <FeatureTable />
-                            </>
-                            : <p className='message'>
-                                {get('panel.no_results')} (2)
-                            </p>
-                    }
-                </section>
-            )
+        <section id='panel' className={(nothingToShow) ? 'nothing-to-show' : ''}>
+            {nothingToShow ? (
+                <p className='message'>
+                    {get('panel.no_results')}
+                </p>
+            ) :
+                pointsCount ?
+                    <>
+                        <ReportButton />
+                        <FeatureTable />
+                    </>
+                    : <p className='message'>
+                        {get('panel.no_results')} (2)
+                    </p>
+            }
+        </section>
     );
 };
 
